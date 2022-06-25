b4bf499419a17dffe722cc11bf09dc9fbe4c33a0
Renata Kim <renata_kim@hotmail.com>
2022-06-25 14:35:16 -0300

    exercicio 15

---
+++ Exercicio.cs
@@ -0,0 +1,59 @@
+﻿
+
+using System.Globalization;
+
+namespace Exercicio15
+{
+    public class Exercicio
+    {
+        public Dictionary<Moeda, Cambio> dicCambios = new Dictionary<Moeda, Cambio>()
+        {
+            { Moeda.Real, new Cambio() { Descricao = "Real (BRL)", Taxa = 1, CultureInfo = "pt-BR"} },
+            { Moeda.Dolar, new Cambio() { Descricao = "Dólar dos Estados Unidos (USD)", Taxa = 4.50M, CultureInfo = "us-US" } },
+            { Moeda.Euro, new Cambio() { Descricao = "Euro (EUR)", Taxa = 6.20M, CultureInfo = "en-GB" } },
+            { Moeda.Iene, new Cambio() { Descricao = "Iene (JPY)", Taxa = 0.052M, CultureInfo = "ja-JP" } },
+            { Moeda.Libra, new Cambio() { Descricao = "Libra Esterlina (GBP)", Taxa = 6.95M, CultureInfo = "fr-FR" } },
+            { Moeda.Won, new Cambio() {Descricao = "Won Sul Coreano (KRW)", Taxa = 0.078M, CultureInfo = "ko-KO"} }
+        };
+
+        public void Start()
+        {
+            Console.WriteLine("Converter de : ");
+            MontarMenu();
+            Moeda conversaoDe = Enum.Parse<Moeda>(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("Converter para : ");
+            MontarMenu();
+            Moeda conversaoPara = Enum.Parse<Moeda>(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("Digite o valor que deseja converter: ");
+            decimal valorParaConverter = decimal.Parse(Console.ReadLine());
+
+            decimal valorConvertido = 0;
+
+            if (conversaoDe == Moeda.Real)
+            {
+                valorConvertido = Conversao.ConverterParaOutraMoeda(valorParaConverter, dicCambios[conversaoPara].Taxa);
+            }
+            else
+            {
+                decimal valorReal = Conversao.ConverterParaReal(valorParaConverter, dicCambios[conversaoDe].Taxa);
+                valorConvertido = Conversao.ConverterParaOutraMoeda(valorReal, dicCambios[conversaoPara].Taxa);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine($"O valor convertido é {valorConvertido.ToString("C", CultureInfo.CreateSpecificCulture(dicCambios[conversaoPara].CultureInfo))}");
+        }
+        public void MontarMenu()
+        {
+            foreach (var item in dicCambios)
+            {
+                Console.WriteLine($"{ (int) item.Key } - {item.Value.Descricao}");
+            }
+
+            Console.Write("> ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿
+namespace Exercicio15
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Exercicio exercicio = new Exercicio();
+            exercicio.Start();
+
+           
+            
+   
+
+            //const double moedaUmRealParaDolar = 4.50;
+            //const double moedaUmRealParaEuro = 6.20;
+            //const double moedaUmRealParaIene = 0.052;
+            //const double moedaUmRealParaLibra = 6.95;
+
+            //Console.WriteLine("Converter de : " +
+            //    "\n 1 - Real (BRL)" +
+            //    "\n 2 - Dólar dos Estados Unidos (USD)" +
+            //    "\n 3 - Euro (EUR)" +
+            //    "\n 4 - Iene (JPY)" +
+            //    "\n 5 - Libra Esterlina (GBP)");
+            //double conversaoDe = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Converter para : " +
+            //    "\n 1 - Real (BRL)" +
+            //    "\n 2 - Dólar dos Estados Unidos (USD)" +
+            //    "\n 3 - Euro (EUR)" +
+            //    "\n 4 - Iene (JPY)" +
+            //    "\n 5 - Libra Esterlina (GBP)");
+            //double conversaoPara = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite o valor que deseja converter: ");
+            //double valorParaConverter = double.Parse(Console.ReadLine());
+
+            //if (conversaoDe == 1 && conversaoPara == 2)
+            //{
+            //    Console.WriteLine($"{Conversao.ConverterParaOutraMoeda(valorParaConverter, moedaUmRealParaDolar)}");
+            //}
+            //else if (conversaoDe == 1 && conversaoPara == 3)
+            //{
+            //    Console.WriteLine($"{Conversao.ConverterParaOutraMoeda(valorParaConverter, moedaUmRealParaEuro)}");
+            //}
+            //else if (conversaoDe == 1 && conversaoPara == 4)
+            //{
+            //    Console.WriteLine($"{Conversao.ConverterParaOutraMoeda(valorParaConverter, moedaUmRealParaIene)}");
+            //}
+            //else if (conversaoDe == 1 && conversaoPara == 5)
+            //{
+            //    Console.WriteLine($"{Conversao.ConverterParaOutraMoeda(valorParaConverter, moedaUmRealParaLibra)}");
+            //}
+            //else if (conversaoDe == 2 && conversaoPara == 3)
+            //{
+            //    double valorEmReal = Conversao.ConverterParaReal(valorParaConverter, moedaUmRealParaDolar);
+
+            //    Console.WriteLine($"{Conversao.ConverterParaOutraMoeda(valorEmReal, moedaUmRealParaEuro)}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Opção inválida, por favor digite uma opção válida");
+
+            //}
+        }
+
+    }
+}
